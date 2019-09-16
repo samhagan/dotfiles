ed67e2f03b2baf37cdeb3f69e3d1c3ab4aa4f6c1
Sam Hagan <samuel.hagan@gmail.com>
2019-09-16 07:30:55 -0700

    fix install base16

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ install_tpm(){
 }
 
 install_base16(){
-  if [[ -d ~/.config/base16-shell ]]; then
+  if [[ ! -d ~/.config/base16-shell ]]; then
     git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
   fi
 }
